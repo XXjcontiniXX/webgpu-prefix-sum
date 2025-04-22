8b9f88f64d0407d3be6b0a71f03eff3f75d0743b
jcontini <jamescontini@gmail.com>
2025-04-22 15:35:45 -0700

    fixed up main.js for plot making

--- main.js
+++ main.js
@@ -1,20 +1,25 @@
 // WebGPU bindings in JavaScript
 let deviceID = 0;
-let checkResults = true;
+let checkResults = false;
 
 
 
 //const THREADS = [32, 64, 128, 256]
 
-//const THREADS = [64, 128, 256]
-const THREADS = [128]
-//const WORKGROUPS = [32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768]
-const WORKGROUPS = [128]
-//const BATCH_SIZES = [1, 2, 4]
-const BATCH_SIZES = [2]
-//const PAR_LOOKBACK = [1, 0]
-const PAR_LOOKBACK = [1]
-const VEC_SIZES = {
+const THREADS = [64, 128, 256]
+
+const WORKGROUPS = [32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768]
+
+const BATCH_SIZES = [1, 2, 4]
+
+const PAR_LOOKBACK = [1, 0]
+
+//const THREADS = [128]
+//const WORKGROUPS = [128]
+//const BATCH_SIZES = [2]
+//const PAR_LOOKBACK = [1]
+
+let VEC_SIZES = {
   [1 << 10]: [], [1 << 11]: [], [1 << 12]: [],
   [1 << 13]: [], [1 << 14]: [], [1 << 15]: [],
   [1 << 16]: [], [1 << 17]: [], [1 << 18]: [],
@@ -22,6 +27,9 @@ const VEC_SIZES = {
   [1 << 22]: [], [1 << 23]: [], [1 << 24]: [],
   [1 << 25]: []
 };
+
+
+
 const PER_THREAD_SIZE = 4;
 
 
@@ -259,9 +267,9 @@ async function initBuffers(device, TUNING_CONFIG) {
 
 async function run(device, pipeline, bindGroup, TUNING_CONFIG, buffers) {
   const vec_size = TUNING_CONFIG.numWorkgroups * TUNING_CONFIG.workgroupSize * TUNING_CONFIG.batch_size * PER_THREAD_SIZE;
-  console.log("workgroups: ", TUNING_CONFIG.numWorkgroups)
-  console.log("threads: ", TUNING_CONFIG.workgroupSize)
-  console.log("batch_size: ", TUNING_CONFIG.batch_size)
+  //console.log("workgroups: ", TUNING_CONFIG.numWorkgroups)
+  //console.log("threads: ", TUNING_CONFIG.workgroupSize)
+  //console.log("batch_size: ", TUNING_CONFIG.batch_size)
   const queue = device.queue;
   //const { buffers.ABuffer, buffers.BBuffer, buffers.CBuffer, buffers.CReadBuffer, buffers.DBuffer, buffers.debugBuffer, buffers.debugReadBuffer, buffers.TimestampResolveBuffer, buffers.TimestampReadBuffer } = await initBuffers(device);
 
@@ -313,11 +321,12 @@ async function run(device, pipeline, bindGroup, TUNING_CONFIG, buffers) {
     console.log("Succesful.")
   }else{
     incorrect = 1;
+    console.log("real: ", output[vec_size - 1], "ideal: ", vec_size * TUNING_CONFIG.alt)
     console.log("There was an incorrect value(s).")
   }
 
   const time = timestampOutput[1] - timestampOutput[0];
-  console.log('Execution Time: ', time, 'ticks (ns)');
+  //console.log('Execution Time: ', time, 'ticks (ns)');
   //console.log(typeof(time))
   //console.log("Throughput: ", (vec_size * 4 * 2)/(time), " GBPS\n")
 
@@ -327,9 +336,9 @@ async function run(device, pipeline, bindGroup, TUNING_CONFIG, buffers) {
 
   const throughput = gigabytesTransferred / timeInSeconds
 
-  console.log("Date.now duration: ", duration, "ns?")
+  //console.log("Date.now duration: ", duration, "ns?")
   //console.log("Throughput: ", throughput.toFixed(5), " GBPS");
-  console.log("Throughput: ", throughput, " GBPS");
+  //console.log("Throughput: ", throughput, " GBPS");
   
   document.getElementById("throughput-display").innerText = `Throughput: ${throughput} GBPS`;
 
@@ -346,8 +355,8 @@ async function run(device, pipeline, bindGroup, TUNING_CONFIG, buffers) {
 
 async function main() {
 
-  const ITERS = 1;
-  const WARM_UPS = 0;
+  const ITERS = 2;
+  const WARM_UPS = 2;
   
   for (let i = 0; i < THREADS.length; i++) {
     for (let j = 0; j < WORKGROUPS.length; j++) {
@@ -357,25 +366,31 @@ async function main() {
           continue;
         }
         for (let l = 0; l < PAR_LOOKBACK.length; l++) {
-          let throughput, incorrect = 0;
-          console.log("vec_size: ", size)
+          let incorrect = 0;
+          let throughput = 0;
+          //console.log("vec_size: ", size)
           for (let p = 1; p <= ITERS + WARM_UPS; p++) { // iters PLUS warmups to get warmups
               if (p >= WARM_UPS) {
                 // througput, threads, workgroups, batch_size, par_lookback
                 const [t, inc] = await main_helper(THREADS[i], WORKGROUPS[j], BATCH_SIZES[k], PAR_LOOKBACK[l], p);
                 throughput += t;
                 incorrect += inc;
-                console.log("throughput: ", t, " GBPS")
+                //console.log("throughput: ", t, " GBPS")
               }else{
-                await main_helper(THREADS[i], WORKGROUPS[j], BATCH_SIZES[k], PAR_LOOKBACK[l])
+                await main_helper(THREADS[i], WORKGROUPS[j], BATCH_SIZES[k], PAR_LOOKBACK[l], p)
               }
           }
-          VEC_SIZES[size].push(throughput / ITERS, THREADS[i], WORKGROUPS[j], BATCH_SIZES[k], PAR_LOOKBACK[l], incorrect)
+          VEC_SIZES[size].push([throughput / ITERS, THREADS[i], WORKGROUPS[j], BATCH_SIZES[k], PAR_LOOKBACK[l], incorrect])
         }
       }
     }
   }
-  console.log(VEC_SIZES)
+  
+  for (let i = 10; i < 26; i++) {
+    VEC_SIZES[1 << i].sort((a, b) => b[0] - a[0])
+    console.log("vec_Size: ", 1 << i)
+    console.log(VEC_SIZES[1 << i][0])
+  }
 }
 
 
@@ -435,18 +450,18 @@ async function main_helper(thx, wkrgx, btchsx, plbkx, p) {
     console.error("Device lost:", info.message);
   }); 
 
-  // Check if 'timestamp-query' and 'subgroup' are supported
-  if (adapter.features.has('timestamp-query')) {
-    console.log('Timestamp queries are supported');
-  } else {
-    console.log('Timestamp queries are not supported on this device.');
-  }
+  // // Check if 'timestamp-query' and 'subgroup' are supported
+  // if (adapter.features.has('timestamp-query')) {
+  //   console.log('Timestamp queries are supported');
+  // } else {
+  //   console.log('Timestamp queries are not supported on this device.');
+  // }
 
-  if (adapter.features.has('subgroups')) {
-    console.log('Subgroup operations are supported');
-  } else {
-    console.log('Subgroup operations are not supported on this device.');
-  }
+  // if (adapter.features.has('subgroups')) {
+  //   console.log('Subgroup operations are supported');
+  // } else {
+  //   console.log('Subgroup operations are not supported on this device.');
+  // }
 
   // Dummy error callback for uncaptured WebGPU errors
   device.onuncapturederror = (event) => {
@@ -460,7 +475,7 @@ async function main_helper(thx, wkrgx, btchsx, plbkx, p) {
   const pipeline = await initComputePipeline(device, bindGroupLayout, TUNING_CONFIG);
   // Run the compute pass (dispatching the work to the GPU)
   let [throughput, incorrect] = await run(device, pipeline, bindGroup, TUNING_CONFIG, buffers);
-  console.log("tp: ", throughput, "inc: ", incorrect)
+  //console.log("tp: ", throughput, "inc: ", incorrect)
   return [throughput, incorrect];
 }
 
